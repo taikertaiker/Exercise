9978f447a70655bd94c385fb4f8ce9099446eb45
taikertaiker <smallkuro@gmail.com>
2020-07-27 11:38:52 +0800

    Hello.java 修改

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hellow word");
+System.out.println("Hellow word edit 2");
 	}
 
 }
