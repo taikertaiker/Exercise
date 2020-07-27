8bcd6cfc62f3c74fbde8c649222b5e8ede968618
taikertaiker <smallkuro@gmail.com>
2020-07-27 13:36:29 +0800

    this is test for commit branch

--- Hello.java
+++ Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hellow word edit 2");
+System.out.println("Hellow word edit 2  to DEV");
 	}
 
-}
+// branch dev
+	}
